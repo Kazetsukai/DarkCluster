30d8545e24b8dd6ee0fab4f3c6d4c2c75617c27d
sinaptik <sin486@gmail.com>
2014-04-22 17:33:29 +1200

    simple button drawing

---
+++ DarkCluster/Assets/GuiController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GuiController : MonoBehaviour {
+
+	List<string> _enemies = new List<string>();
+
+	// Use this for initialization
+	void Start () 
+	{
+		_enemies.Add("ship 1");
+		_enemies.Add("ship 2");
+		_enemies.Add("ship 3");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	void OnGUI ()
+	{
+		DrawEnemySelectors(_enemies);
+	}
+
+	void DrawEnemySelectors(List<string> enemies)
+	{
+		var location = 0;
+
+		foreach (var enemy in enemies)
+		{
+			if (GUI.Button (new Rect (10, 10 + (location * 40), 150, 30), enemy))
+			{
+				Debug.Log("Fired at " + enemy);
+			}
+
+			location++;
+		}
+	}
+}
